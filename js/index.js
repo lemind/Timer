47904264d9d98765c0cee9c6192e6382dc977951
Lyomin Dmitriy <leminds@gmail.com>
2014-02-24 21:17:41 +0400

    add tags select2 to task lines

--- js/index.js
+++ js/index.js
@@ -20,6 +20,46 @@ $(function(){
 		main_button_stop = 	(function() { return $(".main button.stop"); }),
 		main_button_pause = $(".main button.pause");
 
+	$('html').click(function() {
+		console.log('all popups remove');
+		if ($(".project-select2").select2('data').length > 0) {
+			$(".project-select2").remove();
+		}
+
+		if ($(".tags-select2").select2('data').length > 0) {
+			task_tags_update($(".tags-select2").parent().attr("task_id"), $(".tags-select2").select2('data'));
+			$(".tags-select2").remove();
+		}
+	});
+
+	function task_tags_update(task_id, selected_tags) {
+
+		var tags_ids_arr = [];
+
+		selected_tags.forEach(function(tag) {
+			if (tag.fl != 'new') tags_ids_arr.push(tag.id);
+		});
+
+		if (last_new_tags.length) {
+			tags_ids_arr = tags_ids_arr.concat(last_new_tags);	
+			last_new_tags = [];
+		}
+
+		tasks.get(task_id).set({
+			tags: tags_ids_arr.join()
+		}).save(null, {
+		    success: function (model, response) {
+				console.log('task / tags update success');
+				console.log(response);
+				var taskListView = new TaskListView({tasks: tasks, projects: projects, tags: tags});
+		    },
+		    error: function (model, response) {
+		        console.log("error: task save");
+		    }
+		});
+
+	};
+
 	function projects_select2_init () {
 
 		projects.fetch({
@@ -45,7 +85,7 @@ $(function(){
 
 						var new_project = new Project({
 							name: e.object.name,
-							color: 1 + Math.floor(Math.random() * 9)
+							color: Math.floor(Math.random() * 9)
 						});
 
 						new_project.save(null, {
@@ -96,17 +136,13 @@ $(function(){
 
 						var new_tag = new Tag({
 							name: e.object.name,
-							color: 1 + Math.floor(Math.random() * 9)
+							color: Math.floor(Math.random() * 9)
 						});
 
 						new_tag.save(null, {
 						    success: function (model, response) {
 						    	tags.add(new Tag(response));
 						    	last_new_tags.push(response.id);
-						    	console.log('last_new_tag');
-						    	console.log(last_new_tags);
-						    	console.log(e);
-						    	console.log(select2_tags.select2('data'));
 						    },
 						    error: function (model, response) {
 						        console.log("error: save new tag");
@@ -166,11 +202,13 @@ $(function(){
 	var Timer = Backbone.View.extend({
 		el: timer, 
 		events: {
-			"click .main .start": 	"start", 
-			"click .main .stop": 	"stop", 
-			"click .main .pause": 	"pause", 
-			"click .tasks .start": 	"old_task_start",
-			"click .tasks .delete":	"delete_task" 
+			"click .main .start": 		"start", 
+			"click .main .stop": 		"stop", 
+			"click .main .pause": 		"pause", 
+			"click .tasks .start": 		"old_task_start",
+			"click .tasks .delete":		"delete_task",
+			"click .tasks .project":	"edit_project",
+			"click .tasks .tags":		"edit_tags" 
 		},
 		initialize: function() {
 
@@ -335,17 +373,17 @@ $(function(){
 
 			var el_task_line = $(ev.target).parent().parent();
 
-			select_task_id = el_task_line.attr("id");
+			selected_task_id = el_task_line.attr("id");
 
-			if(select_task_id == current_task_id) {
+			if(selected_task_id == current_task_id) {
 				timer_status && this.stop();
 			}
 
 			if (confirm('Are you sure you want to delete this task?')) {
 
-				tasks.get(select_task_id).destroy({
+				tasks.get(selected_task_id).destroy({
 				    success: function (model, response) {
- 						$(".task" + select_task_id).remove();
+ 						$(".task" + selected_task_id).remove();
 				    },
 				    error: function (model, response) {
 				        console.log("error delete task");
@@ -354,6 +392,146 @@ $(function(){
 			}
 
 		},
+		edit_project: function (ev) {
+			ev.stopPropagation();
+			$(".project-select2").remove();
+
+			var el_task_line = $(ev.target).parent();
+
+			selected_task_id = el_task_line.attr("id");			
+
+			var task = tasks.get(selected_task_id);
+
+			$(ev.target).append('<div class="project-select2"></div>');
+
+			$(".project-select2").select2({
+				placeholder: "Select project",
+				quietMillis: 100,
+				data: function() { return {results: projects.toJSON()}; },
+				formatResult: function(post) {
+					return post.name;
+				},
+				formatSelection: function(post) {
+					return post.name;
+				},
+			    createSearchChoice: function (term) {
+			        return { id: 0, name: term };
+			    }
+			}).on("select2-selecting", function(e) {
+
+				var selected_project = e.object;
+
+				if (e.val == 0) {
+
+					var new_project = new Project({
+						name: e.object.name,
+						color: Math.floor(Math.random() * 9)
+					});
+
+					new_project.save(null, {
+					    success: function (model, response) {
+					    	projects.add(new Project(response));
+
+					        last_new_project = response;
+					    },
+					    error: function (model, response) {
+					        console.log("error: save new project");
+					    }
+					});
+				}
+
+				setTimeout(function() {
+					selected_project.id == 0 ? selected_project = last_new_project : selected_project;
+
+					task.set({
+						project_id:	selected_project.id
+					}).save(null, {
+					    success: function (model, response) {
+							console.log('task project update');
+					    },
+					    error: function (model, response) {
+					        console.log("error: task project update");
+					    }
+					});
+
+					$(".task" + selected_task_id + " .project-select2").remove();
+
+					var taskListView = new TaskListView({tasks: tasks, projects: projects, tags: tags});
+				}, 200);
+
+			});
+
+			$(".task" + selected_task_id + " .project-select2").select2('data', {id: task.get('project_id'), name: projects.get(task.get('project_id')).get('name')});
+
+		},
+		edit_tags: function (ev) {
+			ev.stopPropagation();
+			$(".tags-select2").remove();
+
+			var tags_ids = [],
+				current_task_tags = [];
+
+			var el_task_line = $(ev.target);
+
+			//todo fix it
+			if (el_task_line.attr("task_id")) {
+				selected_task_id = el_task_line.attr("task_id");
+				el_task_line.append('<div class="tags-select2"></div>');
+			} else {
+				selected_task_id = el_task_line.parent().attr("task_id");
+				el_task_line.parent().append('<div class="tags-select2"></div>');
+			}
+
+			var task = tasks.get(selected_task_id);
+
+			$(".tags-select2").select2({
+				placeholder: "Select tags",
+				quietMillis: 100,
+				multiple: true,
+				data: function() { return {results: tags.toJSON()}; },
+				formatResult: function(post) {
+					return post.name;
+				},
+				formatSelection: function(post) {
+					return post.name;
+				},
+			    createSearchChoice: function (term) {
+			        return { id: term, name: term, fl: 'new' };
+			    }
+			}).on("select2-selecting", function(e) {
+
+				if (e.object.fl == 'new') {
+
+					var new_tag = new Tag({
+						name: e.object.name,
+						color: Math.floor(Math.random() * 9)
+					});
+
+					new_tag.save(null, {
+					    success: function (model, response) {
+					    	tags.add(new Tag(response));
+					    	last_new_tags.push(response.id);
+					    },
+					    error: function (model, response) {
+					        console.log("error: save new tag");
+					    }
+					});
+				}
+
+			});
+
+
+			if (task.get('tags')) {
+				tags_ids = task.get('tags').split(',');
+
+				tags_ids.forEach(function(id) {
+					current_task_tags.push(tags.get(id).toJSON());
+				});
+
+				$(".task" + selected_task_id + " .tags-select2").select2("data", current_task_tags);
+			}
+
+		}
 
 	});
 
