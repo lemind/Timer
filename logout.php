6331834761350e7c1184b0ec3f6575b388c1a80a
Andrej Antonov <polymorphm@gmail.com>
2014-12-02 23:47:30 +0700

    check http-referor before doing logout request

--- logout.php
+++ logout.php
@@ -1,5 +1,16 @@
 <?php
 
+if (isset($_SERVER['HTTP_REFERER']) && isset($_SERVER['HTTP_HOST'])) {
+    // check http-referor before doing this GET request
+    
+    $p_url = parse_url($_SERVER['HTTP_REFERER']);
+    if ($p_url['host'] != $_SERVER['HTTP_HOST']) {
+        header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+        header('Content-Type: text/plain;charset=utf-8');
+        die('error: bad http referer');
+    }
+}
+
 session_start();
 unset($_SESSION['user']); 
 header('Location: /');
